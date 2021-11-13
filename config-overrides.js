50ad158a5e8bd36eaaa00959e83086c06ce90294
Mina Fayek <minaf65@gmail.com>
2021-11-13 17:12:31 +0200

    Updated engines

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const {
+    override,
+    addBabelPlugin,
+ } = require("customize-cra");
+ module.exports = override(
+    addBabelPlugin(["@babel/plugin-proposal-optional-chaining", { "loose": false }]),
+    addBabelPlugin(["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": false }]),
+    addBabelPlugin(["@babel/plugin-syntax-bigint", { "loose": false }])
+ );
